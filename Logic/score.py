b3d37958c9ef6b6b98aff0785aa6655d25a325ef
Никита <nikitacepikov7@gmail.com>
2023-06-01 23:45:33 +0300

    Building a system

---
+++ Logic/score.py
@@ -0,0 +1,18 @@
+from Entity.product import Product
+from Container.basket import Basket
+
+
+class ShopAssistance:
+    @staticmethod
+    def calculate_total_price(basket):
+        if isinstance(basket, Basket) and basket.size != 0:
+            total = 0
+            for i in range(basket.size):
+                product = basket.get_product(i)
+
+                if isinstance(product, Product):
+                    total += product.price
+
+            return total
+        else:
+            return 0
---
+++ Points/bronze.py
@@ -0,0 +1,33 @@
+from Points.coins import Coin
+
+
+class Orange(Coin):
+    def __init__(self, price=):
+        super().__init__(price)
+
+        self.__diameter = diameter if diameter > 0 else 100
+        self.__vitamin = vitamin if vitamin >= 0 else 1000
+
+
+    @property
+    def diameter(self):
+        return self.__diameter
+
+    @diameter.setter
+    def diameter(self, diameter):
+        if diameter > 0:
+            self.__diameter = diameter
+
+    @property
+    def vitamin(self):
+        return self.__vitamin
+
+    @vitamin.setter
+    def vitamin(self, vitamin):
+        if vitamin >= 0:
+            self.__vitamin = vitamin
+
+    def __str__(self):
+        return (f"Orange: Diameter = {self.__diameter},"
+                f" Vitamin C = {self.__vitamin},"
+                f" Price = ${self.price}")
