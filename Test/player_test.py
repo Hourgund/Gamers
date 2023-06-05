b7ac35c5ae090c4bbc93bc81b90133ba4d093d25
Никита <nikitacepikov7@gmail.com>
2023-06-05 13:19:55 +0300

    Understood how to realize my logic

---
+++ Test/player_test.py
@@ -0,0 +1,61 @@
+import unittest
+from Containers.player import Players
+
+
+class BronzeTest(unittest.TestCase):
+    def test_bronze_default_constructor(self):
+        bronze = Bronze()
+        expected_weight = 1
+        expected_price = 0
+
+        self.assertEqual(expected_weight, bronze.weight)
+        self.assertEqual(expected_price, bronze.price)
+
+    def test_bronze_constructor_with_args(self):
+        expected_weight = 10
+        expected_price = 5
+
+        bronze = Bronze(expected_weight, expected_price)
+
+        self.assertEqual(expected_weight, bronze.weight)
+        self.assertEqual(expected_price, bronze.price)
+
+    def test_negative_weight_bronze(self):
+        weight = -10
+        expected = 10
+        bronze = Bronze(weight=weight)
+        self.assertEqual(expected, bronze.weight)
+
+    def test_zero_weight_bronze(self):
+        weight = 0
+        expected = 0
+        bronze = Bronze(weight=weight)
+        self.assertEqual(expected, bronze.weight)
+
+    def test_zero_price_bronze(self):
+        price = 0
+        expected = 0
+        bronze = Bronze(price=price)
+        self.assertEqual(expected, bronze.bronze)
+
+    def test_weight_property_negative_bronze(self):
+        bronze = Bronze()
+        expected = bronze.weight
+        bronze.weight = -20
+        self.assertEqual(expected, bronze.weight)
+
+    def test_weight_property_positive_bronze(self):
+        bronze = Bronze()
+        expected = 10
+        bronze.weight = 8
+        self.assertEqual(expected, bronze.weight)
+
+    def test_weight_property_with_zero_bronze(self):
+        bronze = Bronze()
+        expected = bronze.weight
+        bronze.weight = 0
+        self.assertEqual(expected, bronze.weight)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- Util/main.py
+++ Util/main.py
@@ -7,28 +7,45 @@ from Logic.table_of_leaders import Table
 
 
 def main():
-    player = Players()
+    player1 = Players()
+    player2 =Players()
 
     br = Bronze(1, 10.0)
     sil = Silver(5, 15.0)
     gol = Gold(10, 20.0)
 
-    player.add(sil)
-    player.add(gol)
-    player.add(sil)
-    player.add(gol)
-    player.add(sil)
-    player.add(gol)
+    player1.add(sil)
+    player1.add(gol)
+    player1.add(sil)
+    player1.add(gol)
+    player1.add(sil)
+    player1.add(gol)
 
-    print(f"size = {player.size}")
+    player2.add(sil)
+    player2.add(gol)
+    player2.add(sil)
+    player2.add(gol)
 
-    print(player)
+    print(f"size = {player1.size}")
+    print(f"size = {player2.size}")
 
-    total = Score.calculate_total_price(player)
+    print(player1)
+    print(player1)
 
-    print(f"Total price = {total}")
 
-    Min_total = Table.find_min_total()
+    total1 = Score.calculate_total_price(player1)
+    total2 = Score.calculate_total_price(player2)
+
+
+
+    print(f"Total price = {total1}")
+    print(f"Total price = {total2}")
+
+    Min_total = Table.find_min_total(total2, total1)
+    Max_total = Table.find_max_total(total2, total1)
+
+    print(f"The biggest resoult is ", Max_total)
+    print(f"The smallest resoult is ", Min_total)
 
 if __name__ == "__main__":
     main()
